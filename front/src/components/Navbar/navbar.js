6cf5fda627b21611fc44548fd34ba69cad2e63bc
Julie Brosse <privatejuliedev@gmail.com>
2020-05-17 11:56:41 +0200

    conflicts solved

--- front/src/components/Navbar/navbar.js
+++ front/src/components/Navbar/navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React , {Link, Fragment} from 'react';
 import './navbar.css';
 import { withStyles } from '@material-ui/core/styles';
@@ -123,58 +122,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-=======
-import React , { Component } from 'react';
-import {Link} from 'react-router-dom';
-
-
-import './navbar.css';
-class Navbar extends Component {
-  // componentDidMount() {
-  //   window.addEventListener("scroll", this.resizeHeaderOnScroll);
-  // }
-  // resizeHeaderOnScroll() {
-  //   const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-  //     shrinkOn = 100,
-  //     headerEl = document.getElementById("header-profile__wrapper"),
-  //     logoEl = document.getElementsByClassName("header-profile__logo"),
-  //     //headerMenu = document.getElementsByClassName("header-menu"),
-  //     rootid = document.getElementById("root");
-  //     if (distanceY > shrinkOn) {
-  //     headerEl.classList.add("smaller");
-  //     logoEl[0].classList.add("smaller");
-  //     /*
-  //     if (headerMenu.length >= 1){
-  //       headerMenu[0].classList.add("marginfix");
-  //     }
-  //     */
-  //   } else {
-  //     logoEl[0].classList.remove("smaller");
-  //     /*
-  //     if (headerMenu.length >= 1){
-  //       headerMenu[0].classList.remove("marginfix");
-  //     }
-  //     */
-  //     rootid.classList.remove("marginfix");
-  //   }
-  // }
-
-  // process.env.NODE_ENV === 'production' ? "https://zen-shaw-4b92a9.netlify.com/" : "//localhost:3000/"
-  render() {
-    return (
-    <div id="header-profile">
-      <div id="header-profile__wrapper">
-          <div className="navbarRow">
-                <Link to="/" className="header-profile__logo">
-                      <figure className="header-profile__figure" alt="La Vanguardia">
-                          <img src="https://rsc.lavanguardia.com/img/logo-image-v1000486.svg" className="img-responsive" alt="La Vanguardia" rel="logo"></img>
-                      </figure>
-                </Link>
-          </div>
-      </div>
-    </div>
-)
-}
-}
-export default Navbar
->>>>>>> d176a9ed7667484b39ea89f2661e230a264276aa
